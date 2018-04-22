2c159aba5c06ab34a03b0550a9610741534e2885
reeckset <henriquemlima7@gmail.com>
2018-04-22 03:29:36 +0100

    Did connect screen on android. Fully implemented (might add warnings for invalid ips or connections)

---
+++ LPOORTAL_Game_Client/core/src/com/lpoortal/game/controller/KeyboardController.java
@@ -0,0 +1,57 @@
+package com.lpoortal.game.controller;
+
+import com.lpoortal.game.LPOORTAL_Game;
+import com.lpoortal.game.network.MessageManager;
+
+public class KeyboardController {
+
+    private String input;
+    private long ip;
+
+    public KeyboardController(){
+        input = "";
+    }
+
+    public void addKey(int i){
+        if(input.length() < 8) {
+            ip += i * Math.pow(16, 7-input.length());
+            input += Integer.toHexString(i).toUpperCase();
+        }
+    }
+
+    public void erase(){
+        if(input.length() > 0) {
+            input = input.substring(0, input.length() - 1);
+            ip -= ip % Math.pow(16, 8-input.length());
+        }
+    }
+
+    public void submit(){
+        if(input.length() == 8) {
+            String hostname = "";
+            for (int i = 0; i < 4; i++) {
+                long tmp = ip;
+                tmp >>>= i * 8;
+                tmp &= 0xFF;
+                hostname = tmp + ((hostname == "") ? "" : ("." + hostname));
+            }
+            System.out.println(hostname);
+            MessageManager.getInstance().start(hostname);
+            LPOORTAL_Game.getInstance().changeState(LPOORTAL_Game.State.DRAWING_STATE);
+        }else{
+            System.out.println("Hostname must be input fully");
+        }
+    }
+
+    public int getValAt(int i){
+        long tmp = ip;
+        tmp >>>= (7-i) * 4;
+        tmp &= 0xF;
+        return (int) tmp;
+    }
+
+    @Override
+    public String toString(){
+        return input;
+    }
+}
